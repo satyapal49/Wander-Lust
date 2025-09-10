167b9838602fbba08fe1f7ba34193699c969360c
Satyapal Raj <satyapalraj49@gmail.com>
2025-09-10 22:33:43 +0530

    feat: restructure application by separating routes for listings and reviews, and improve schema validation

--- Schema.js
+++ Schema.js
@@ -1,18 +1,17 @@
 const Joi = require('joi');
 
-const listingSchema = Joi.object({
+module.exports.listingSchema = Joi.object({
     listing : Joi.object({
-        tittle : Joi.string().required(),
+        title : Joi.string().required(),
         description : Joi.string().required(),
         country : Joi.string().required(),
         location : Joi.string().required(),
-        image : Joi.string().required(),
+        image : Joi.string(),
         price : Joi.number().required(),
 
     }).required()
 })
 
-module.exports = listingSchema;
 
 module.exports.reviewSchema = Joi.object({
     review : Joi.object({
---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const ExpressError = require("./utils/ExpressError.js");
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
+
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/wanderlust');
+    console.log('Connected to MongoDB');
+}
+
+
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+
+app.use("/listings", listings)
+app.use("/listings/:id/reviews", reviews)
+
+// error handing middleware
+
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
+});
+
+
+app.use((err, req, res, next)=>{
+    let {status = 501, message = "Something went wrong"} = err;
+    res.status(status).render("error.ejs", {err})
+});
+
+app.listen(8080, () => {
+    console.log('Server is running on http://localhost:8080');
+    main().catch(err => console.log(err));
+});
