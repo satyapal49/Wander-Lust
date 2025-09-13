54bdfeee2a43e8e960eacc6fb624ce318f5b0d45
Satyapal Raj <satyapalraj49@gmail.com>
2025-09-13 21:22:48 +0530

    feat: add session management using express-session middleware

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const ExpressError = require("./utils/ExpressError.js");
 const cookieParser = require('cookie-parser');
+const session = require("express-session");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -23,6 +24,15 @@ app.set("views", path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, '/public')));
+
+
+const sessionOptions = { 
+    secret: "mysupersecretstring",
+    resave: false,
+    saveUninitialized: true,
+};
+
+app.use(session(sessionOptions));
 
 
 app.use("/listings", listings)
