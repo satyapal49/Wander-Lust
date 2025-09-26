f1b9366a9898ce1ae87d113a6cf33ed11365701a
Satyapal Raj <satyapalraj49@gmail.com>
2025-09-26 13:50:06 +0530

    refactor: update environment variable handling and add .gitignore for sensitive files

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto : {
-        secret: "mysupersecretstring"
+        secret: process.env.SECRET,
     },
     touchAfter:  24 * 3600,
 });
@@ -52,7 +52,7 @@ store.on("error", (res, req)=>{
 
 const sessionOptions = { 
     store: store,
-    secret: "mysupersecretstring",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie : {
