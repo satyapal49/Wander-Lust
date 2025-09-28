b6aa0e9cc1010f419fe22da6b6047774498f0063
Satyapal Raj <satyapalraj49@gmail.com>
2025-09-28 08:48:54 +0530

    Increase initial zoom level on the map to enhance visibility

--- public/js/map.js
+++ public/js/map.js
@@ -4,7 +4,7 @@ const map = new mapboxgl.Map({
     container: "map", // container ID
     style: "mapbox://styles/mapbox/streets-v12",
     center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 11 // starting zoom
+    zoom: 12 // starting zoom
 });
 
 const marker = new mapboxgl.Marker({ color: "red" })
